ea004c9e90bb304825ba1f43c4b83ee7e3263253
Natalia Milani <natalia.milani@hotmail.com>
2021-09-07 18:43:31 -0300

    Ajustes e Melhorias

--- src/main/java/com/impacta/microservices/credito/credito/controller/CreditoController.java
+++ src/main/java/com/impacta/microservices/credito/credito/controller/CreditoController.java
@@ -2,16 +2,16 @@ package com.impacta.microservices.credito.credito.controller;
 
 import com.impacta.microservices.credito.credito.domain.Credito;
 import com.impacta.microservices.credito.credito.repository.CreditoRepository;
-import com.impacta.microservices.credito.credito.service.CreditoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 //import java.net.InetAddress;
 //import java.net.UnknownHostException;
@@ -24,29 +24,22 @@ public class CreditoController {
     @Autowired
     private CreditoRepository repository;
 
-    /*
-    private final CreditoService creditoService;
-
-    public CreditoController(CreditoService creditoService) {
-        this.creditoService = creditoService;
-    }
-    */
-
-    @GetMapping(path = "/status_aplicattion")
-    public String check(){
+    @GetMapping(path = "/status_aplicacao")
+    public String checarStatusAplicacao(){
         return "Aplicação online";
-    }
-
-    @GetMapping(path = "/consulta/{id_transacao}")
-    public  ResponseEntity<Credito> consultarCredito (@PathVariable("id_transacao") Integer id_transacao ){
-        return repository.findById(id_transacao)
-        .map(record -> ResponseEntity.ok().body(record))
-        .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping(path = "/creditar_valor")
     public Credito salvar(@RequestBody Credito credito){
         return repository.save(credito);
+    }
+
+    //Tratativa de erro quando não localizar o id_transacao
+    @GetMapping(path = "/consulta/{id_transacao}")
+    public Credito consultarCredito (@PathVariable("id_transacao") Integer id_transacao ){
+
+        return  repository.findById(id_transacao)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Crédito não encontrado para o id informado."));
     }
 
     /*
